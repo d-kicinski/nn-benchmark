58e255a4f1a1fdd587324249aa38415d678932a2
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-06-02 12:32:54 +0200

    Add lazy instantiation for models to save some vram

--- src/benchmark.py
+++ src/benchmark.py
@@ -4,30 +4,29 @@ import json
 from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 import time
 import torch
 
-from models import MobileNetV1, MobileNetV2, mobilenet_v3_small, mobilenet_v3_large
-from models import resnet18, resnet34, resnet50
-from models import shufflenet_v2_x0_5, shufflenet_v2_x1_0
+from models import LazyModel
 
-torch.set_num_threads(4)
+torch.set_num_threads(1)
 
 
 class ExperimentProvider:
-    def __init__(self, name: str, model: torch.nn.Module, data: Data):
-        self._name = name
-        self._model = model
+    def __init__(self, lazy_model: LazyModel, data: Data):
+        self._lazy_model = lazy_model
         self._data = data
 
         self._loss = torch.nn.CrossEntropyLoss()
         self._dummy_labels = torch.zeros((data.train.shape[0],), dtype=torch.long)
 
+        self._model: Optional[torch.nn.Module] = None
+
     @property
     def name(self) -> str:
-        return self._name
+        return self._lazy_model.name
 
     @property
     def data(self) -> Data:
@@ -40,17 +39,37 @@ class ExperimentProvider:
 
     @property
     def model(self) -> torch.nn.Module:
+        if self._model is None:
+            self._model = self._lazy_model()
         return self._model
 
     @model.setter
     def model(self, model: torch.nn.Module):
         self._model = model
 
+    def initialize_model(self):
+        if self._model is None:
+            self._model = self._lazy_model()
+
+    def assign_device(self, device: str):
+        self._dummy_labels = self._dummy_labels.to(device)
+        self._data.assign_device(device)
+        if self._model is None:
+            raise ValueError("Explicitly initialize model with `initialize_model` or `model`")
+        else:
+            self._model = self._model.to(device)
+
+    def clear(self):
+        del self._model
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+        self.model = None
+
     def step_once(self) -> None:
-        self._model(self._data.eval)
+        self.model(self._data.eval)
 
     def train_step_once(self) -> None:
-        out = self._model(self._data.train)
+        out = self.model(self._data.train)
         loss = self._loss(out, self._dummy_labels)
         loss.backward()
 
@@ -64,7 +83,7 @@ class BenchmarkResult:
 
 
 def _throughput(start, end, steps):
-    return 1 / (1e-7 + (end - start) / steps)
+    return 1 / (1e-7 + ((end - start) / steps))
 
 
 def benchmark(experiment: ExperimentProvider, steps: int) -> BenchmarkResult:
@@ -78,6 +97,9 @@ def benchmark(experiment: ExperimentProvider, steps: int) -> BenchmarkResult:
         experiment.train_step_once()
     end_time_train = time.time()
 
+    # start_time_train = 0
+    # end_time_train = 1
+
     return BenchmarkResult(experiment.name,
                            experiment.data.eval.shape[2],
                            _throughput(start_time_step, end_time_step, steps),
@@ -85,7 +107,7 @@ def benchmark(experiment: ExperimentProvider, steps: int) -> BenchmarkResult:
 
 
 class Runner:
-    RUN_DELAY_SECONDS = 3.0
+    RUN_DELAY_SECONDS = 10.0
 
     def __init__(self, device: str):
         self._device = device
@@ -94,10 +116,12 @@ class Runner:
             -> List[BenchmarkResult]:
         results: List[BenchmarkResult] = []
         for e in experiments:
+            e.initialize_model()
+            e.assign_device(self._device)
+            e.train_step_once()  # dry run for sanity-check
             time.sleep(Runner.RUN_DELAY_SECONDS)
-            e.model = e.model.to(self._device)
-            e.data = Data(e.data.eval.to(self._device), e.data.train.to(self._device))
             r = benchmark(e, steps)
+            e.clear()
             results.append(r)
             if verbose:
                 print(f"{r.name}: {r.forward_step:.4f} {r.train_step:.4f}")
@@ -115,9 +139,13 @@ class Data:
     eval: torch.Tensor
     train: torch.Tensor
 
+    def assign_device(self, device: str):
+        self.eval = self.eval.to(device)
+        self.train = self.train.to(device)
+
 
 if __name__ == '__main__':
-    steps = 3
+    steps = 10
     batch_size = 8
     sizes = [64, 124, 160, 224]
 
@@ -137,17 +165,17 @@ if __name__ == '__main__':
     for data, size in zip(datas, sizes):
         print(f"=== {size} ===")
         experiments = [
-            ExperimentProvider("MobileNet(1.0)", MobileNetV1(3, 1000, 1.0), data),
-            ExperimentProvider("MobileNet(0.5)", MobileNetV1(3, 1000, 0.5), data),
-            ExperimentProvider("MobileNetV2(1.0)", MobileNetV2(width_mult=1.0), data),
-            ExperimentProvider("MobileNetV2(0.5)", MobileNetV2(width_mult=0.5), data),
-            ExperimentProvider("MobileNetV3(Large)", mobilenet_v3_large(), data),
-            ExperimentProvider("MobileNetV3(Small)", mobilenet_v3_small(), data),
-            ExperimentProvider("ShuffleNetV2(1.0)", shufflenet_v2_x1_0(), data),
-            ExperimentProvider("ShuffleNetV2(0.5)", shufflenet_v2_x0_5(), data),
-            ExperimentProvider("ResNet18", resnet18(), data),
-            ExperimentProvider("ResNet34", resnet34(), data),
-            ExperimentProvider("ResNet50", resnet50(), data),
+            ExperimentProvider(LazyModel("MobileNetV1(1.0)"), data),
+            ExperimentProvider(LazyModel("MobileNetV1(0.5)"), data),
+            ExperimentProvider(LazyModel("MobileNetV2(1.0)"), data),
+            ExperimentProvider(LazyModel("MobileNetV2(0.5)"), data),
+            ExperimentProvider(LazyModel("MobileNetV3(Large)"), data),
+            ExperimentProvider(LazyModel("MobileNetV3(Small)"), data),
+            ExperimentProvider(LazyModel("ShuffleNetV2(1.0)"), data),
+            ExperimentProvider(LazyModel("ShuffleNetV2(0.5)"), data),
+            ExperimentProvider(LazyModel("ResNet18"), data),
+            ExperimentProvider(LazyModel("ResNet34"), data),
+            ExperimentProvider(LazyModel("ResNet50"), data),
         ]
 
         if torch.cuda.is_available():
@@ -159,12 +187,11 @@ if __name__ == '__main__':
         print("Running on CPU")
         results = cpu_runner.run_experiments(experiments)
         update_stats_(cpu_stats, results)
-
-        print()
         print()
 
-    with Path("benchmark_cpu.json").open("w") as fp:
+    with Path("../results/benchmark_cpu.json").open("w") as fp:
         json.dump(cpu_stats, fp, indent=4)
 
-    with Path("benchmark_gpu.json").open("w") as fp:
-        json.dump(cpu_stats, fp, indent=4)
+    if torch.cuda.is_available():
+        with Path("../results/benchmark_gpu.json").open("w") as fp:
+            json.dump(gpu_stats, fp, indent=4)
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,45 @@
+import torch
+
 from .mobilenetv1 import *
 from .mobilenetv2 import *
 from .mobilenetv3 import *
 from .resnet import *
 from .shufflenetv2 import *
+
+
+class LazyModel:
+    def __init__(self, name: str):
+        self._name = name
+
+    def __call__(self) -> torch.nn.Module:
+        return self._construct_model_by_name()
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def _construct_model_by_name(self) -> torch.nn.Module:
+        if self._name == "MobileNetV1(1.0)":
+            return MobileNetV1(3, 1000, alpha=1.0)
+        elif self._name == "MobileNetV1(0.5)":
+            return MobileNetV1(3, 1000, alpha=0.5)
+        if self._name == "MobileNetV2(1.0)":
+            return MobileNetV2(width_mult=1.0)
+        elif self._name == "MobileNetV2(0.5)":
+            return MobileNetV2(width_mult=0.5)
+        if self._name == "MobileNetV3(Large)":
+            return mobilenet_v3_large()
+        elif self._name == "MobileNetV3(Small)":
+            return mobilenet_v3_small()
+        if self._name == "ShuffleNetV2(0.5)":
+            return shufflenet_v2_x0_5()
+        elif self._name == "ShuffleNetV2(1.0)":
+            return shufflenet_v2_x1_0()
+        elif self._name == "ResNet18":
+            return resnet18()
+        elif self._name == "ResNet34":
+            return resnet34()
+        elif self._name == "ResNet50":
+            return resnet50()
+        else:
+            raise ValueError(f"Model {self._name} is unknown!")
