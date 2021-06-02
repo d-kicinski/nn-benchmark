c6abdb6e01c64dd770ef7021c6aadeddff71082a
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-06-02 14:10:24 +0200

    Save figures on disk

--- src/benchmark_plot.py
+++ src/benchmark_plot.py
@@ -2,18 +2,18 @@ import json
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-DATA_PATH = "benchmark_cpu.json"
-
 markers = ["s", "d", "^", "v", "x", "X", "h", "P", "p", "*", "<", ">", "1"]
 
+BLACKLIST = ["ResNet18", "ResNet34", "ResNet50"]
 
-def plot_benchmark():
-    with Path(DATA_PATH).open("r") as fp:
+def plot_benchmark(input_path: str, output_path: str, run_type: str = "forward", show: bool = True):
+    with Path(input_path).open("r") as fp:
         stats = json.load(fp)
     sizes = set()
-    print(len(stats.items()))
     for i, (name, data) in enumerate(stats.items()):
-        data = data["forward"]
+        if name in BLACKLIST:
+            continue
+        data = data[run_type]
         x = list(map(int, data.keys()))
         y = list(map(float, data.values()))
         plt.plot(x, y, markers[i], label=f"{name}")
@@ -23,8 +23,14 @@ def plot_benchmark():
     plt.legend(numpoints=1, loc="upper left", bbox_to_anchor=(1.04, 1))
     plt.tight_layout()
     plt.xticks(list(sizes))
-    plt.show()
+    plt.savefig(output_path)
+
+    if show:
+        plt.show()
 
 
 if __name__ == '__main__':
-    plot_benchmark()
+    plot_benchmark("../results/benchmark_cpu.json", "../results/cpu_forward.png", "forward")
+    plot_benchmark("../results/benchmark_cpu.json", "../results/cpu_train.png", "backward")
+    plot_benchmark("../results/benchmark_gpu.json", "../results/gpu_forward.png", "forward")
+    plot_benchmark("../results/benchmark_gpu.json", "../results/gpu_train.png", "backward")
